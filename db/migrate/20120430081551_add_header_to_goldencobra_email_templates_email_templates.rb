82893f37c1d865758e2bff5215691656a0ebfa9d
Marco Metz <marco.metz@gmail.com>
2012-04-30 11:06:22 +0200

    deleted:    db/migrate/20120430081551_add_header_to_goldencobra_email_templates_email_templates.rb 	modified:   test/dummy/db/schema.rb

--- db/migrate/20120430081551_add_header_to_goldencobra_email_templates_email_templates.rb
+++
@@ -1,6 +0,0 @@
-class AddHeaderToGoldencobraEmailTemplatesEmailTemplates < ActiveRecord::Migration
-  def change
-    add_column :goldencobra_email_templates_email_templates, :header_html, :text
-    add_column :goldencobra_email_templates_email_templates, :footer_html, :text
-  end
-end
