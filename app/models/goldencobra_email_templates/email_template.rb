db09e707df6eacfefa6f34c01d71ec856c8ba277
Holger Frohloff <holger.frohloff@gmail.com>
2012-05-10 14:07:48 +0200

    annotate

--- app/models/goldencobra_email_templates/email_template.rb
+++ app/models/goldencobra_email_templates/email_template.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: goldencobra_email_templates_email_templates
+#
+#  id           :integer(4)      not null, primary key
+#  title        :string(255)
+#  content_txt  :text
+#  content_html :text
+#  bcc          :string(255)
+#  custom_css   :text
+#  layout_file  :string(255)
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#  subject      :string(255)
+#
+
 module GoldencobraEmailTemplates
   class EmailTemplate < ActiveRecord::Base
     attr_accessible :bcc, :content_html, :content_txt, :custom_css, :layout_file, :title, :subject
